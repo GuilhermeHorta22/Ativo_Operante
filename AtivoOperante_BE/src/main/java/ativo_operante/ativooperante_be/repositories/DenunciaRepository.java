bbc72ecc27d0d897d73620f8174d59405145edb1
GuilhermeHorta22 <ghorta800@gmail.com>
2025-05-02 18:55:49 -0300

    crud feedback feito

--- AtivoOperante_BE/src/main/java/ativo_operante/ativooperante_be/repositories/DenunciaRepository.java
+++ AtivoOperante_BE/src/main/java/ativo_operante/ativooperante_be/repositories/DenunciaRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface DenunciaRepository extends JpaRepository<Denuncia, Long> {
-    //findByUsuario -> não sei o motivo disso
+    //findByUsuario -> seria para eu conseguir buscar uma denuncia que tenha um usuario com aquele id
     @Query(value = "INSERT INTO feedback(fee_texto,den_id) VALUES (:fee_texto, :den_id)", nativeQuery = true)
     public boolean addFeedback(@Param("den_id") Long id, @Param("fee_texto") String texto);
 }
