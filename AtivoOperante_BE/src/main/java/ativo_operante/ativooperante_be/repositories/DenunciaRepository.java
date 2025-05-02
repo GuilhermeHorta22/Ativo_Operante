154ae6ae99533c94d29e488222c11e0c792098dc
GuilhermeHorta22 <ghorta800@gmail.com>
2025-05-02 12:00:44 -0300

    pasta para o front-end

--- AtivoOperante_BE/src/main/java/ativo_operante/ativooperante_be/repositories/DenunciaRepository.java
+++ AtivoOperante_BE/src/main/java/ativo_operante/ativooperante_be/repositories/DenunciaRepository.java
@@ -2,7 +2,11 @@ package ativo_operante.ativooperante_be.repositories;
 
 import ativo_operante.ativooperante_be.entities.Denuncia;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface DenunciaRepository extends JpaRepository<Denuncia, Long> {
     //findByUsuario -> não sei o motivo disso
+    @Query(value = "INSERT INTO feedback(fee_texto,den_id) VALUES (:fee_texto, :den_id)", nativeQuery = true)
+    public boolean addFeedback(@Param("den_id") Long id, @Param("fee_texto") String texto);
 }
