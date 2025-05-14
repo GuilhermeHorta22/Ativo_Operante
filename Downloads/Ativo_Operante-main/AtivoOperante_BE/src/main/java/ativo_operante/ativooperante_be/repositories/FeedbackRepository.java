7f831b03e7a58337a6ef764c48decfad715e0156
Caio Emerick <caiopros@hotmail.com>
2025-05-14 11:21:31 -0300

    Remove pastas desnecessárias do repositório

--- Downloads/Ativo_Operante-main/AtivoOperante_BE/src/main/java/ativo_operante/ativooperante_be/repositories/FeedbackRepository.java
+++
@@ -1,7 +0,0 @@
-package ativo_operante.ativooperante_be.repositories;
-
-import ativo_operante.ativooperante_be.entities.Feedback;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
-}
--- Downloads/Ativo_Operante-main/AtivoOperante_BE/src/main/java/ativo_operante/ativooperante_be/repositories/OrgaoRepository.java
+++
@@ -1,7 +0,0 @@
-package ativo_operante.ativooperante_be.repositories;
-
-import ativo_operante.ativooperante_be.entities.Orgao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrgaoRepository extends JpaRepository<Orgao, Long> {
-}
