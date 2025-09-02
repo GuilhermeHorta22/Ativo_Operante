ff82f4cbbb4e3ef89c3c354e183a467e72398ac1
joao <martinezclaudio610@gmail.com>
2025-09-02 15:32:59 -0300

    removendo antigo back-end

--- AtivoOperante_BE/src/main/java/ativo_operante/ativooperante_be/repositories/OrgaoRepository.java
+++
@@ -1,9 +0,0 @@
-package ativo_operante.ativooperante_be.repositories;
-
-import ativo_operante.ativooperante_be.entities.Orgao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrgaoRepository extends JpaRepository<Orgao, Long> {
-}
